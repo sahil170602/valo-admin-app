29e56b26afe281ce929978fe8af2748ea26e2e3c
Sahil <your-sahilmeshram1917@gm.com>
2026-08-29 02:19:13 +0530

    Add GitHub cloud build

--- electron/main.cjs
+++ electron/main.cjs
@@ -11,12 +11,9 @@ function createWindow() {
       contextIsolation: false
     }
   });
-
   win.loadFile(path.join(__dirname, '../dist/index.html'));
 }
-
 app.whenReady().then(createWindow);
-
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
